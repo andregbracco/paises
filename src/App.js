f8546711d7b616a07f93b0b9dd845d399627b462
andregbracco <andygbracco@gmail.com>
2021-10-28 17:20:28 -0300

    paises

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
+import {useState} from 'react';
 import './App.css';
+import paises from './datos/paises.js';
 
 function App() {
+  const[codeSeleccion, setCodeSeleccion] = useState();
+  const[datosPaises, setDatosPaises] = useState(paises)
+
+  function tomarValor(evento){
+    setCodeSeleccion(evento.target.value);
+  }
+
+  function buscar(){
+    let elegido = datosPaises.find(pais => pais.code === codeSeleccion)
+    elegido['poblacion'] = 100;
+    return elegido.name + " - " + elegido.poblacion;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <select onChange={tomarValor} >
+            {paises.map((pais)=> <option value={pais['code']} key={pais['name']}>{pais['name']}</option>)}
+        </select>
+        <hr/>
+        <p>{codeSeleccion && buscar()}</p>
     </div>
   );
 }
---
+++ src/datos/paises.js
@@ -0,0 +1,19 @@
+
+export default [
+  {
+    name: 'AFGHANISTAN',
+    code: 'afg',
+  }, {
+    name: 'ALBANIA',
+    code: 'alb',
+  }, {
+    name: 'TURKMENISTAN',
+    code: 'tkm',
+  }, {
+    name: 'TUVALU',
+    code: 'tuv',
+  }, {
+    name: 'UGANDA',
+    code: 'uga',
+  },
+];
