1f9b2c59a9be25955101222dea2dd9840c8e525e
andregbracco <andygbracco@gmail.com>
2021-10-29 14:03:10 -0300

    agregar poblacion y eliminar card

--- src/App.js
+++ src/App.js
@@ -4,26 +4,56 @@ import './App.css';
 import paises from './datos/paises.js';
 
 function App() {
-  const[codeSeleccion, setCodeSeleccion] = useState();
+
+  const[codeSeleccion, setCodeSeleccion] = useState(0);
   const[datosPaises, setDatosPaises] = useState(paises)
+  const[poblacion, setPoblacion] = useState('')
 
   function tomarValor(evento){
+    //toma el valor del select y setea 'codeSeleccion' con ese valor
     setCodeSeleccion(evento.target.value);
   }
 
-  function buscar(){
+  function buscar(){     
+    //busca name por 'codeSeleccion'
     let elegido = datosPaises.find(pais => pais.code === codeSeleccion)
-    elegido['poblacion'] = 100;
-    return elegido.name + " - " + elegido.poblacion;
+    if(elegido) {
+      return (elegido.name).toLowerCase() ;}
+      else return 'l pais que selecciones'
+  }
+
+  function tomarPoblacion(evento){
+    //setea poblacion, busca al 'codeSeleccion' y le asigna el atributo 'poblacion'
+    setPoblacion(evento.target.value)
+    let elegido = datosPaises.find(pais => pais.code === codeSeleccion)
+    elegido.poblacion = evento.target.value;
+    setDatosPaises(datosPaises.sort((a,b)=> a.poblacion - b.poblacion))
+  }
+
+  function eliminar(dato){
+    setDatosPaises(datosPaises.filter(pais=> pais !== dato))
   }
 
   return (
     <div className="App">
-        <select onChange={tomarValor} >
-            {paises.map((pais)=> <option value={pais['code']} key={pais['name']}>{pais['name']}</option>)}
-        </select>
-        <hr/>
-        <p>{codeSeleccion && buscar()}</p>
+      <h1>TEST PAISES</h1>
+      <hr/><br/>
+      <h2>Selecciona el país: </h2>
+      <select onChange={tomarValor} >
+        <option></option>
+          {datosPaises.map((pais)=> <option value={pais['code']} key={pais['name']}>{pais['name']}</option>)}
+      </select><br/><br/>
+      
+
+      <label>Ingresar población de {buscar()}: </label>
+      <input onChange={tomarPoblacion} type="number"></input>
+
+
+      <ul>
+      
+      {datosPaises.map(pais => <li key={pais['name']} >{pais.name}  -  {(pais.poblacion) ? pais.poblacion : 'sin cambios'}<button onClick={()=>eliminar(pais)} >x</button></li>)}
+      </ul>
+
     </div>
   );
 }
